1cc84259bcc78c26eb2bad154df0309ef3e4f7d8
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-05-20 18:50:25 +0800

    Konfigurasikan server Vite untuk pengembangan lokal dengan dukungan HTTPS dan HMR

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import fs from 'fs';
+
+const host = 'pt-lsl.test';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: fs.readFileSync(`/Users/denisdjadianardika/Library/Application Support/Herd/config/valet/Certificates/${host}.key`),
+            cert: fs.readFileSync(`/Users/denisdjadianardika/Library/Application Support/Herd/config/valet/Certificates/${host}.crt`),
+        }
+    }
 });
